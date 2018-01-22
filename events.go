40389ae8f582c96629668270cc003cc86ef02ef2
setyven <setyven@gmail.com>
2018-01-22 19:15:42 +0700

    Update events.go

--- events.go
+++ events.go
@@ -10,7 +10,7 @@ import (
 	"reflect"
 	"time"
 
-	"github.com/jetbasrawi/go.geteventstore/internal/uuid"
+	"github.com/setyven/go.geteventstore/internal/uuid"
 )
 
 // EventResponse encapsulates the response for an event reflecting the atom
